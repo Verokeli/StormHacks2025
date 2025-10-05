da0120b09f2cd508c0129ee7a7747f409320008e
Hyelim Kim <hyelim2100@gmail.com>
2025-10-04 19:06:46 -0700

    change hotspot

--- false-creek-planet/src/components/Hostpot.jsx
+++ false-creek-planet/src/components/Hostpot.jsx
@@ -1,20 +1,36 @@
-
-import React from 'react';
+import React from "react";
 
 export default function Hotspot({ s, onOpen }) {
     return (
         <button
             type="button"
             className="hotspot"
-            style={{ left: s.pos.x + '%', top: s.pos.y + '%', transform: 'translate(-50%, -50%)' }}
-            onClick={onOpen}
+            onClick={() => onOpen?.(s)}
+            aria-label={s.name}
+            style={{
+                left: s.pos.x + "%",
+                top: s.pos.y + "%",
+                transform: "translate(-50%, -50%)",
+                position: "absolute",
+                background: "transparent",
+                border: "none",
+                cursor: "pointer",
+                padding: 0,
+            }}
         >
-            <div className="glyph"
+            { }
+            <img
+                src={s.silhouette}
+                alt={s.name}
+                className="glyph"
                 style={{
-                    WebkitMaskImage: `url(${s.silhouette})`,
-                    maskImage: `url(${s.silhouette})`
+                    width: "80px",
+                    height: "80px",
+                    display: "block",
                 }}
             />
+
+            { }
             <div className="label">{s.name}</div>
         </button>
     );
