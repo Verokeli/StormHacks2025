219474d5af498b9f09906693f601e2c19621a8c7
Veronica.Sheng <veronica0616@VeronicaShengs-MacBook-Pro.local>
2025-10-05 09:05:47 -0700

    Updated UI: restyled Open Chat button, hid Then/Now toggle, and adjusted SVG positioning

--- false-creek-planet/src/pages/Scene.jsx
+++ false-creek-planet/src/pages/Scene.jsx
@@ -52,7 +52,7 @@ export default function Scene() {
         <main className={`scene ${isPast ? 'scene--past' : 'scene--present'}`}>
             <header className="scene__header">
                 <h1>WHEN THE SEA<br />REMEMBERED.</h1>
-                <Toggle isPast={isPast} setIsPast={setIsPast} />
+                {/* <Toggle isPast={isPast} setIsPast={setIsPast} /> */}
             </header>
 
             {/* hotspots (positioned absolutely using s.pos.x / s.pos.y) */}
